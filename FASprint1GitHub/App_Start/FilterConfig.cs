b7edb236253a2c2af549ce8434d36f5a0faac04c
Mrudula Narkhede <MrudulaNarkhede@outlook.com>
2013-12-27 11:56:21 -0800

    initial commit

---
+++ FASprint1GitHub/App_Start/FilterConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FASprint1GitHub
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+
+        }
+    }
+}
---
+++ FASprint1GitHub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FASprint1GitHub.MvcApplication" Language="C#" %>
